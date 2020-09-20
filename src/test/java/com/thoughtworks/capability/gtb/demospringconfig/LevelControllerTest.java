70cb1bf36535af89ea1e513cdf44a17c02f90ab4
MSI-PV <805560812@qq.com>
2020-09-20 23:50:31 +0800

    complete problem2-1

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LevelControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Value("${levelNumber}")
+    private int levelNumber;
+
+    @Test
+    void shouldReturnBasicWhenLevelNumberIsLessThanOne() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(jsonPath("$",is("basic")))
+                .andExpect(status().isOk());
+    }
+
+}
